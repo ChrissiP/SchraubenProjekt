8a751c2ed156e466cc69b36d30286cef6c90f44e
KarinMatea <karin.pavic@docc.techstarter.de>
2023-07-07 21:17:13 +0200

    Update mit Wuerth
    Wuerth fertig + ein bissl css angepasst!

--- public/wuerth.js
+++ public/wuerth.js
@@ -17,8 +17,8 @@ async function createChart() {
     datasets: [{
       label: 'Gesamtumsatz Würth',
       data: datasetData,
-      backgroundColor: 'rgba(54, 162, 235, 0.2)',
-      borderColor: 'rgba(54, 162, 235, 1)',
+      backgroundColor:'rgba(255, 99, 132, 0.2)',
+      borderColor: 'rgba(255, 99, 132, 1)',
       borderWidth: 1
     }]
   };
